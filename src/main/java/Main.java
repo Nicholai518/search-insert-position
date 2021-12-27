a08084efc9ac75916de0e06c915c35d557837617
Nicholas <n.paz4444@gmail.com>
2021-12-27 13:47:25 -0500

    Successfully completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 	public static void main(String[] args){
-	int[] nums = new int[]{1, 3, 5, 6};
+	int[] nums = new int[]{1, 1, 1, 1};
 	int target = 2;
+
+	System.out.println("Example one. Answer should be 1: " + solution(nums, target));
 
 	}
 
@@ -9,15 +11,23 @@ public class Main {
 
 		// edge case - what if nums length is zero
 		// return 0
-
+		if(nums.length == 0){
+			return 0;
+		}
 
 		// for loop - iterate through nums
+		for(int i=0; i<nums.length; i++){
 
-		// possible simplier solution
-		// if - index is greater than or equal to target
-		// return index
+			// if - index is greater than or equal to target
+			// return index
+			if(nums[i] >= target){
+				return i;
+			}
+		}
 
+		// if you make it this far, all the elements are less than target
+		// return nums.length to indicate this value should be added to the very end after all elements
+		return nums.length;
 
-		return 0;
 	}
 }
