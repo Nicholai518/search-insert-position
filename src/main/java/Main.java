18a65e32a78c3cc9f406d2c5aac4c29c8cc2faa7
Nicholas <n.paz4444@gmail.com>
2021-12-27 13:39:06 -0500

    algorithm created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
 public class Main {
 	public static void main(String[] args){
+	int[] nums = new int[]{1, 3, 5, 6};
+	int target = 2;
 
+	}
+
+	public static int solution(int[] nums, int target){
+
+		// edge case - what if nums length is zero
+		// return 0
+
+
+		// for loop - iterate through nums
+
+		// possible simplier solution
+		// if - index is greater than or equal to target
+		// return index
+
+
+		return 0;
 	}
 }
